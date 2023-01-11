0401945277c5ac9ee12fce9763bb77ee07063e4c
unknown <exezick143@gmail.com>
2023-01-11 11:02:02 +0800

    add todo counter in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import FetchData from './FetchData';
 
 function Footer() {
+  const {
+    data,
+  } = FetchData();
+
   return (
     <div className="row">
         <label htmlFor="all">
           <input type="checkbox" name="all" id="all" />
           All
         </label>
-        <p>You have 0 to do</p>
+        <p>You have {data.length} to do</p>
         <button id="delete">Delete</button>
     </div>
   )
