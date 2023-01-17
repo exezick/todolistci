e7068537d434ff0722e5f01be27449ba744e0573
unknown <exezick143@gmail.com>
2023-01-17 19:38:48 +0800

    determine the done todos

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -3,11 +3,18 @@ import React from 'react'
 function ListItem({name, complete, id}) {
   return (
     <li>
-        <label htmlFor={id}>
-            <input type="checkbox" id={id} />
+      {
+        complete === "true"
+        ? <label htmlFor={id} className="active"> 
+            <input type="checkbox" id={id} defaultChecked={true} />
             {name}
-        </label>
-        <button>Edit</button>
+          </label>
+        : <label htmlFor={id}> 
+            <input type="checkbox" id={id}  />
+            {name}
+          </label>
+      }
+        <button id={id}>Edit</button>
     </li>
   )
 }
